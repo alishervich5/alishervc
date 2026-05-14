9a7cbbbe88e1ca88205e63f3b5a20d3dd1b598b4
Behruz-Coder <uzb8972@gmail.com>
2026-05-14 15:25:03 +0500

    Initial commit

---
+++ src/com/company/Exception1.java
@@ -0,0 +1,18 @@
+public class Exception1 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a - b;
+        System.out.println("sum = " + sum);
+
+        if  (a == b) {
+            System.out.println("a == b");
+        } else {
+            System.out.println("a != b");
+        }
+        try {
+
+        }
+    }
+}
